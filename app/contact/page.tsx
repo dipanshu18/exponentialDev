9d6ebe4666b358bd0d90ea7b881d695c81aefd89
dipanshu18 <torawanedipanshu@gmail.com>
2024-02-05 09:02:24 +0530

    Contact page modified

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactUsForm from "@/components/ContactUsForm";
 
 export default function page() {
   return (
-    <section id="contact-us" className="py-16">
+    <section id="contact-us" className="py-16 min-h-screen">
       <h1 className="text-center font-extrabold text-4xl lg:text-5xl mb-14">
         Contact us
       </h1>
